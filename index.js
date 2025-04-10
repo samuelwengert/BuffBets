4c1c6dcb5a26a5799d8f59996aa0e61e43d67483
Abdulrahman Almutairi <abal8802@colorado.edu>
2025-04-09 20:06:05 -0600

    Removed the duplicate code in the index.js file

--- index.js
+++ index.js
@@ -231,5 +231,5 @@ app.get('/odds', async (req, res)=> {
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-module.exports = app.listen(3000);app.listen(3000);
+module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
