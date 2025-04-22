d312bdb8956886d0bb03168c1c0fa926c1562da7
dbakula-1 <daba3586@colorado.edu>
2025-04-22 14:32:55 -0600

    Added DB URL to init_db.sh

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -4,7 +4,7 @@
 # This file contains sensitive information and should be kept private
 
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="<DATABASE_EXTERNAL_URL>"
+PG_URI="postgresql://exampleuser:Q3xM4d2I7NwDB9WuCNlW82EPXHP81MQT@dpg-d00onni4d50c73chmtmg-a/eampledb"
 
 # Execute each .sql file in the directory
 for file in init_data/*.sql; do
